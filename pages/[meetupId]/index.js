fdff91a619046d5e6f6d6b3422f6c668dd7a539c
SanOP360 <sanjayl2001alm@gmail.com>
2024-02-12 19:22:18 +0530

    added getStaticPaths

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,14 +1,15 @@
 import MeetupDetails from "../../components/meetups/meetupDetails";
 
-
-const MeetupDetailsPage = (props) => {
+const MeetupDetailsPage = () => {
   return (
     <MeetupDetails
-      meetupData={props.meetupData} 
+      img="https://picsum.photos/id/1050/200/300"
+      title="A first meetup"
+      address="some street 345 new Coast"
+      description="the Meetup Description"
     />
   );
 };
-
 export async function getStaticPaths() {
   return {
     fallback: false,
@@ -16,19 +17,19 @@ export async function getStaticPaths() {
       { params: { meetupId: "m1" } },
       { params: { meetupId: "m2" } },
       { params: { meetupId: "m3" } },
-      { params: { meetupId: "m4" } },
     ],
   };
 }
 
 export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
+  console.log(meetupId);
   return {
     props: {
       meetupData: {
-        id: meetupId,
         img: "https://picsum.photos/id/1050/200/300",
-        title: "A first meetup",
+        id: meetupId,
+        title: "first meetup",
         address: "some street 345 new Coast",
         description: "the Meetup Description",
       },
