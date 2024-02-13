0d4f0da0f06a75e31820ce80e76c60e7ef6925af
SanOP360 <sanjayl2001alm@gmail.com>
2024-02-13 12:02:46 +0530

    removed hard coded values from details page

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,38 +1,59 @@
 import MeetupDetails from "../../components/meetups/meetupDetails";
+import { MongoClient,ObjectId } from "mongodb";
 
-const MeetupDetailsPage = () => {
+const MeetupDetailsPage = (props) => {
   return (
     <MeetupDetails
-      img="https://picsum.photos/id/1050/200/300"
-      title="A first meetup"
-      address="some street 345 new Coast"
-      description="the Meetup Description"
+      img={props.meetupData.image}
+      title={props.meetupData.title}
+      address={props.meetupData.address}
+      description={props.meetupData.description}
     />
   );
 };
+
 export async function getStaticPaths() {
+  const client = await MongoClient.connect(
+    "mongodb+srv://sanjay360:odFRO8cneh3V7B9l@cluster0.mj4clg1.mongodb.net/meetups"
+  );
+  const db = client.db();
+
+  const meetupCollection = db.collection("meetups");
+
+  const meetups = await meetupCollection.find({}, { _id: 1 }).toArray();
+
+  client.close();
+
   return {
     fallback: false,
-    paths: [
-      { params: { meetupId: "m1" } },
-      { params: { meetupId: "m2" } },
-      { params: { meetupId: "m3" } },
-    ],
+    paths: meetups.map((meetup) => ({
+      params: { meetupId: meetup._id.toString() },
+    })),
   };
 }
 
 export async function getStaticProps(context) {
   const meetupId = context.params.meetupId;
-  console.log(meetupId);
+  const client = await MongoClient.connect(
+    "mongodb+srv://sanjay360:odFRO8cneh3V7B9l@cluster0.mj4clg1.mongodb.net/meetups"
+  );
+  const db = client.db();
+
+  const meetupCollection = db.collection("meetups");
+
+  const selectedMeetups = await meetupCollection.findOne({_id: new ObjectId(meetupId)});
+
+  client.close();
+
   return {
     props: {
       meetupData: {
-        img: "https://picsum.photos/id/1050/200/300",
-        id: meetupId,
-        title: "first meetup",
-        address: "some street 345 new Coast",
-        description: "the Meetup Description",
-      },
+        id:selectedMeetups._id.toString(),
+        title:selectedMeetups.data.title,
+        address:selectedMeetups.data.address,
+        image:selectedMeetups.data.image,
+        description:selectedMeetups.data.description
+      }
     },
   };
 }
